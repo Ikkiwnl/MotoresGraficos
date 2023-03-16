49bc47342c664985042dcb9f1399a79b001f593f
Ikkiwnl <107694677+Ikkiwnl@users.noreply.github.com>
2023-03-16 12:56:46 -0600

    Update DepthStencilView.h

--- Tutorial07/Include/DepthStencilView.h
+++ Tutorial07/Include/DepthStencilView.h
@@ -8,25 +8,25 @@ Device;
 class
 DepthStencilView{
 public:
-			DepthStencilView() = default;
-			~DepthStencilView() { SAFE_RELEASE(m_pDepthStencilView) };
+	DepthStencilView() = default;
+	~DepthStencilView() { SAFE_RELEASE(m_pDepthStencilView) };
 
-			// Crea una vista de depth stencil
-			//
+	// Crea una vista de depth stencil
+	
 
-			void
-			init(Device device, ID3D11Resource* depthStencil, DXGI_FORMAT Format);
+	void
+	init(Device device, ID3D11Resource* depthStencil, DXGI_FORMAT Format);
 
-			void
-			update();
+	void
+	update();
 
-			void
-			render();
+	void
+	render();
 
-			void
-			destroy();
+	void
+	destroy();
 
 public:
-			ID3D11DepthStencilView* m_pDepthStencilView = nullptr;
+	ID3D11DepthStencilView* m_pDepthStencilView = nullptr;
 
 };
