b23a28911bfe786d4cb7c12f495fd65ab4262ca0
Ikkiwnl <107694677+Ikkiwnl@users.noreply.github.com>
2023-03-16 12:55:48 -0600

    Update Window.h

--- Tutorial07/Include/Window.h
+++ Tutorial07/Include/Window.h
@@ -2,52 +2,52 @@
 #include <Windows.h>
 
 class
-	Window
+Window
 {
 public:
-			Window();
-			~Window();
-			/* Para crear y mostrar la ventana
-			* hInstance: Identifica una instancia o modulo
-			* nCmdShow:  nCmdShow parametro se usa para minimizar y maximizar la ventana.
-			* wndproc:  es un puntero a una función definida por la aplicación denominada 
-			* procedimiento de ventana o proceso de ventana. 
-			* windowName:	string que identifica la clase window.
-			*/
+	Window();
+	~Window();
+	/* Para crear y mostrar la ventana
+	* hInstance: Identifica una instancia o modulo
+	* nCmdShow:  nCmdShow parametro se usa para minimizar y maximizar la ventana.
+	* wndproc:  es un puntero a una funciÃ³n definida por la aplicaciÃ³n denominada 
+	* procedimiento de ventana o proceso de ventana. 
+	* windowName:	string que identifica la clase window.
+	*/
 
-			HRESULT
-			init(HINSTANCE hInstance, int nCmdShow, WNDPROC wndproc, LPCSTR windowName);
+	HRESULT
+	init(HINSTANCE hInstance, int nCmdShow, WNDPROC wndproc, LPCSTR windowName);
 
-			void
-			update();
+	void
+	update();
 
-			void
-			render();
+	void
+	render();
 
-			void
-			destroy();
+	void
+	destroy();
 
 	//Erick Aaron :D
 public:
-			//El parametro hwnd es el identificador de window devuelto por CreateWindowEx.
-			HWND								m_hWnd			=		nullptr;
+	//El parametro hwnd es el identificador de window devuelto por CreateWindowEx.
+	HWND m_hWnd = nullptr;
 
 			//hInstance en WNDCLASSEX especifica las instancia sobre la cual la clase esta registrada.
-			HINSTANCE						m_hInst			=		nullptr;
+	HINSTANCE m_hInst = nullptr;
 
 
-			RECT								m_rect;
+	RECT m_rect;
 
-			//obtenemos el ancho de la ventana
+	//obtenemos el ancho de la ventana
 
-			unsigned int				m_width;
+	unsigned int				m_width;
 
-			//obtenemos el alto de la ventana
+	//obtenemos el alto de la ventana
 
-			unsigned int				m_height;
+	unsigned int				m_height;
 
-			//obtenemos el nombre de la ventana
+	//obtenemos el nombre de la ventana
 
-			LPCSTR m_windowName							=		"None";
+	LPCSTR m_windowName							=		"None";
 };
 //Erick Aaron :D
