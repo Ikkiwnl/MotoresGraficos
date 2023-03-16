cc4b904f36c5c4d95a408623d76b0e0f2b4e68ba
Ikkiwnl <107694677+Ikkiwnl@users.noreply.github.com>
2023-03-16 13:03:44 -0600

    Update RenderTargetView.h

--- Tutorial07/Include/RenderTargetView.h
+++ Tutorial07/Include/RenderTargetView.h
@@ -9,33 +9,33 @@ Texture;
 class 
 RenderTargetView{
 public:
-			RenderTargetView() = default;
-			~RenderTargetView() {};
+	RenderTargetView() = default;
+	~RenderTargetView() {};
 
-			/*
-			* Creamos un Render Target View para entrar a la resource data,
-			* device: Referencia para llamar al metodo CreateRenderTargetView
-			* backBuffer: Textura creada como render target
-			* Format: Le da el desc.Formt del Render target view desc
-			*/
-			void
-			init(Device device, 
-						Texture backBuffer, 
-						DXGI_FORMAT Format);
+	/*
+	* Creamos un Render Target View para entrar a la resource data,
+	* device: Referencia para llamar al metodo CreateRenderTargetView
+	* backBuffer: Textura creada como render target
+	* Format: Le da el desc.Formt del Render target view desc
+	*/
+	void
+	init(Device device, 
+		Texture backBuffer, 
+		DXGI_FORMAT Format);
 
-			void
-			update();
+	void
+	update();
 
-			void
-			render();
+	void
+	render();
 
-			void
-			destroy();
+	void
+	destroy();
 
 public:
-			//La rendertargeview identifica los recursos del render target a los que 
-			//se puede entrar durante el render
-			ID3D11RenderTargetView* m_renderTargetView = nullptr;
+	//La rendertargeview identifica los recursos del render target a los que 
+	//se puede entrar durante el render
+	ID3D11RenderTargetView* m_renderTargetView = nullptr;
 
 };
 //Erick Aaron :D
